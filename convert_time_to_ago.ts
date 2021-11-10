10ac5b04a41d01abdea6a384997e7fe72aef794e
P <69576826+phatnguyen1006@users.noreply.github.com>
2021-11-10 16:17:56 +0700

    convert time to timestamp ago

---
+++ convert_time_to_ago.ts
@@ -0,0 +1,33 @@
+const convertTime = (createdAt: Date): string => {
+  let createdDate: any = new Date(createdAt);
+
+  let dateString: string = _formatTime(Date.parse(createdDate)).toString() + " ago";
+
+  return dateString;
+};
+
+function _formatTime(timeCreated: number) {
+  var periods = {
+    month: 30 * 24 * 60 * 60 * 1000,
+    week: 7 * 24 * 60 * 60 * 1000,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+  };
+
+  var diff = Date.now() - timeCreated;
+
+  if (diff > periods.month) {
+    // it was at least a month ago
+    return Math.floor(diff / periods.month) + "m";
+  } else if (diff > periods.week) {
+    return Math.floor(diff / periods.week) + "w";
+  } else if (diff > periods.day) {
+    return Math.floor(diff / periods.day) + "d";
+  } else if (diff > periods.hour) {
+    return Math.floor(diff / periods.hour) + "h";
+  } else if (diff > periods.minute) {
+    return Math.floor(diff / periods.minute) + "m";
+  }
+  return "Just now";
+}
