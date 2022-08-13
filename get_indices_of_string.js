14065a24180ba56d7c405cd5e9979b9b78371d61
P <69576826+phatnguyen1006@users.noreply.github.com>
2022-08-13 23:29:43 +0700

    Update get_indices_of_string.js

--- get_indices_of_string.js
+++ get_indices_of_string.js
@@ -1,3 +1,9 @@
+/**
+  * Drawback: cant detect "aa" in "aaa" <=> should return [0,1] but the result is [0], bc it remove string after found.
+  * 
+  * @returns {number[]}
+  */
+
 function getIndicesOf(searchStr, str, caseSensitive) {
     var searchStrLen = searchStr.length;
     if (searchStrLen == 0) {
@@ -13,4 +19,22 @@ function getIndicesOf(searchStr, str, caseSensitive) {
         startIndex = index + searchStrLen;
     }
     return indices;
+}
+
+/**
+  * Solutions:
+  * @returns {number[]} startingIndices
+  */
+
+var str = "...";
+var searchKeyword = "...";
+
+var startingIndices = [];
+
+var indexOccurence = str.indexOf(searchKeyword, 0);
+
+while(indexOccurence >= 0) {
+    startingIndices.push(indexOccurence);
+
+    indexOccurence = str.indexOf(searchKeyword, indexOccurence + 1);
 }
